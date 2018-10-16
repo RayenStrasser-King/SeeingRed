f3cedccac127932a4ca725ff2dd9b74aa84ac5ec
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2018-10-15 17:10:29 -0700

    pixel size

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Bitmap/Bitmap.h"
+#include "Bitmap/bitmap.h"
 #include <iostream>
 #include <vector>
 
@@ -8,6 +8,28 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
+  if (argc != 2)
+  {        
+  cout <<"Please specify one image file!\n";
+  }
+ 
+  string filename = argv[1];
+  Bitmap image;
+  image.open(filename);
+  image.isImage();
+
+  if (image.isImage())
+  {
+    vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
   
+  cout<<argv[1]<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" pixels wide\n";
+
+  }
+  else
+     {
+     cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+     }
+
+
   return 0;
 }
